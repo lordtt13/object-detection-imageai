a1689f28d9fa206998b731d56e4a7184fe41b91b
lordtt13 <thakurtanmay72@yahoo.com>
2020-05-13 10:11:35 +0530

    Video Detect script add
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

---
+++ video-detect.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 09:57:31 2020
+
+@author: tanmay
+"""
+
+import argparse
+
+from imageai.Detection import VideoObjectDetection
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--path',
+    help = 'Path to Video',
+    type = str,
+    required = True
+)
+
+args = parser.parse_args()
+arguments = args.__dict__
+
+detector = VideoObjectDetection()
+detector.setModelTypeAsTinyYOLOv3()
+detector.setModelPath("yolo-tiny.h5?raw=true")
+detector.loadModel()
+
+video_path = detector.detectObjectsFromVideo(input_file_path = arguments.pop("path"),
+                                output_file_path = "new.mp4",
+                                frames_per_second = 20, log_progress = True)
+
+print(video_path)
