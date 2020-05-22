a319f132fc303acca883989a5bbbd5a6bea5ff41
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-05-22 20:41:27 +0530

    Update run.sh to download more models

--- run.sh
+++ run.sh
@@ -6,3 +6,9 @@ pip install tensorflow==1.4.0
 pip install keras==2.1.5
 pip install opencv-python
 pip install imageai
+
+# Get More Models
+# YOLO:
+wget https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5
+# RetinaNET
+wget https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/resnet50_coco_best_v2.0.1.h5
