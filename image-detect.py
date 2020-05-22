99fa2926c8f943b223fc41cd028d6aa75b50e955
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-05-22 20:39:55 +0530

    Argparse extended

--- image-detect.py
+++ image-detect.py
@@ -14,25 +14,46 @@ from imageai.Detection import ObjectDetection
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    '--path',
+    '--load-path',
     help = 'Path to Image',
     type = str,
     required = True
+)
+
+parser.add_argument(
+    '--model',
+    help = 'Model which you want',
+    type = str,
+    default = "YOLO-tiny",
+    options = ["Retinanet", "YOLO", "YOLO-tiny"]
+)
+
+parser.add_argument(
+    '--save-path',
+    help = 'Path to Image',
+    type = str,
+    default = "new.jpg"
 )
 
 args = parser.parse_args()
 arguments = args.__dict__
 
 detector = ObjectDetection()
-detector.setModelTypeAsTinyYOLOv3()
-detector.setModelPath("yolo-tiny.h5")
+
+if arguments.pop('model') == "YOLO":
+    detector.setModelTypeAsYOLOv3()
+    detector.setModelPath("yolo.h5")
+elif arguments.pop('model') == "Retinanet":
+    detector.setModelTypeAsRetinanet()
+    detector.setModelPath("resnet50_coco_best_v2.0.1.h5")
+else:
+    detector.setModelTypeAsTinyYOLOv3()
+    detector.setModelPath("yolo-tiny.h5")
+
 detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image = arguments.pop('path'), \
-                                             output_image_path = "new.jpg", \
+detections = detector.detectObjectsFromImage(input_image = arguments.pop('load-path'), \
+                                             output_image_path = arguments.pop('save-path'), \
                                                  minimum_percentage_probability = 30)
 
 for eachObject in detections:
     print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
-
-
-cv2.imshow('img', cv2.imread('new.jpg'))    
